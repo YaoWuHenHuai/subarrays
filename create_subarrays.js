501593c1d3a994ea39a198230c26eae0f8ab65af
Santiago <57055962+YaoWuHenHuai@users.noreply.github.com>
2024-08-13 21:37:23 -0600

    Create create_subarrays.js

---
+++ create_subarrays.js
@@ -0,0 +1,11 @@
+var subarrays_values = {};
+function createSubarrays(array, size) {
+    const result = [];
+    for (let i = 0; i < array.length; i += size) {
+        result.push(array.slice(i, i + size));
+    }
+    return result;
+}
+array_to_subarray = ["a","b","c","d","e","f"];
+values_per_subarray = 2;
+subarrays_values[i] = (createSubarrays(array_to_subarray, values_per_subarray)).reverse();
